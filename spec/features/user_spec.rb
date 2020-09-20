ec2a7107e370f951c24bc9a77cced6ca845b001a
meu29 <meu29@ezweb.ne.jp>
2020-09-20 19:16:09 +0900

    フォーマット確認

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,13 +1,31 @@
 require "rails_helper"
 
 #bundle exec rspec /home/meu/ドキュメント/RailsInternApp/spec/features/user_spec.rb
+#ログアウトは本来自動で行うので、手動実行でのテストはしない
 
 feature "User", type: :feature do
 
-  scenario "期間の選択後、社員一覧画面が表示されるか" do
-    visit "/period"
-    click_on "決定"
-    expect(page).to have_content("社員番号")
+  scenario "ログインの失敗後、再度ログイン画面を表示" do
+
+    visit "/"
+    click_button "ログイン"
+    expect(page).to have_content("ユーザーIDかパスワードが間違っています")
+
   end
+
+  scenario "ログインの成功後、期間選択画面に遷移" do
+    
+    visit "/"
+    fill_in "user_id", with: "H184100001"
+    fill_in "password", with: "dvbs72bk"
+    click_button "ログイン"
+    expect(page).to have_content("勤怠報告期間選択")
+    Redis.current.del("user_data")
+
+  end
+
+  scenario ""
+
+  
 
 end
